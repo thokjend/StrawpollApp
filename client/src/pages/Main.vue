fad5ea7962839b2ee1c758b17e6ac37e762c45c2
thokjend <thokjend@online.no>
2025-03-04 11:29:07 +0100

    Update Main.vue

--- client/src/pages/Main.vue
+++ client/src/pages/Main.vue
@@ -2,16 +2,33 @@
 import { onMounted, ref } from "vue";
 import { ValidateToken } from "../utils/ValidateToken";
 import Header from "../components/Header.vue";
-import { fetchAllPolls } from "../services/PollService";
+import { fetchAllPolls, fetchSinglePoll } from "../services/PollService";
 
 const polls = ref([]);
+const pollsDetails = ref([]);
 
 onMounted(async () => {
+  await fetchPollId();
   await getPolls();
-  console.log(polls.value);
+  console.log(pollsDetails.value);
 });
 
 const getPolls = async () => {
+  try {
+    // Fetch details for each poll ID
+    const pollData = await Promise.all(
+      polls.value.map(async (id) => {
+        const response = await fetchSinglePoll(id);
+        return response.data; // Extract `data` from the API response
+      })
+    );
+    pollDetails.value = pollData; // Store all fetched poll details
+  } catch (error) {
+    console.log("Error fetching poll details");
+  }
+};
+
+const fetchPollId = async () => {
   try {
     const result = await fetchAllPolls();
     polls.value = result.polls;
